6cb89e9e6aaa70f3b5fbc58999487523b2a4348f
Herman Platou <hsplatou@gmail.com>
2019-10-16 20:49:36 +0200

    push

--- Code/Code/index _stacc.js
+++ Code/Code/index _stacc.js
@@ -1,4 +1,4 @@
-var data_Default=JSON.stringify({
+var data_Default = JSON.stringify({
     laanebelop: 2000000,
     nominellRente: 3,
     terminGebyr: 30,
@@ -6,58 +6,68 @@ var data_Default=JSON.stringify({
     saldoDato: "2020-01-01",
     datoForsteInnbetaling: "2020-02-01",
     ukjentVerdi: "TERMINBELOP"
-  })
+})
 
 
-$('#bnt_soklan').click(function(){
+$('#bnt_soklan').click(function () {
     console.log('getting data from post')
-     getDataFromPost(getDataFromForm())
-  })
+    getDataFromPost(getDataFromForm())
+})
 
-  
-function getDataFromForm(){
-/*get data check if all fieds have data, if not supply*/
-var vBelop = $('#input_lanebelop').val()
-var vNedbetalingstid = $('#input_nedbetalingstid').val()
-var vRente = $('#input_rente').val()
 
-var currDate = new Date().toISOString().slice(0,10);
-var currYear = new Date().toISOString().slice(0,4);
-var utlop = parseInt(currYear) + parseInt(vNedbetalingstid)
-vNedbetalingstid = utlop.toString() + currDate.slice(4,10)
- 
+function getDataFromForm() {
+    var vBelop = $('#input_lanebelop').val()
+    var vNedbetalingstid = $('#input_nedbetalingstid').val()
+    var vRente = $('#input_rente').val()
+    var vAvdragsfrihet = $('#input_avdragsfrihet').val()
+    var currDate = new Date().toISOString().slice(0, 10);
 
-var vData=JSON.stringify({
-    laanebelop: vBelop,
-    nominellRente: 3,
-    terminGebyr: 30,
-    utlopsDato: vNedbetalingstid,
-    saldoDato: currDate,
-    datoForsteInnbetaling: currDate,
-    ukjentVerdi: "TERMINBELOP"
-  })
+    vNedbetalingstid = addYearsToCurrentDate(vNedbetalingstid)
+    vAvdragsfrihet = addYearsToCurrentDate(vAvdragsfrihet)
 
-  return vData
+
+    var vData = JSON.stringify({
+        laanebelop: vBelop,
+        nominellRente: vRente,
+        terminGebyr: 30,
+        utlopsDato: vNedbetalingstid,
+        saldoDato: currDate, ¨
+        datoForsteInnbetaling: currDate,
+        ukjentVerdi: "TERMINBELOP"
+    })
+
+    return vData
+}
+
+function addYearsToCurrentDate(pYear) {
+    if (pYear != null) {
+        var currDate = new Date().toISOString().slice(0, 10);
+        var currYear = new Date().toISOString().slice(0, 4);
+        return parseInt(currYear) + parseInt(pYear) + currDate.slice(4, 10)
+    }
+    else {
+        return new Date().toISOString().slice(0, 10);
+    }
 }
 
 
-function getDataFromPost(pData){
+function getDataFromPost(pData) {
     fetch('https://visningsrom.stacc.com/dd_server_laaneberegning/rest/laaneberegning/v1/nedbetalingsplan', {
-    method: 'POST',
-    body: pData,
-    headers: {
-      "Content-type": "application/json; charset=UTF-8"
-    }
-  })
-  .then(response => response.json())
-  .then(json => console.log(json))
-  .then(console.log('here i would break down the actual json file and send it to its respected methods'))
-  .catch(function(error) {
-    console.log(error)
-  });  
-   }
+        method: 'POST',
+        body: pData,
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+        .then(response => response.json())
+        .then(json => console.log(json))
+        .then(console.log('here i would break down the actual json file and send it to its respected methods'))
+        .catch(function (error) {
+            console.log(error)
+        });
+}
 
-   /*only pass the data that is necessary to creat a single row, then return this*/
-   function createTableRow(pData){
+/*only pass the data that is necessary to creat a single row, then return this*/
+function createTableRow(pData) {
 
-   }
+}
